90e6c0dc34de849142672e284694494168e1814b
alfredamos <alfredamos@gmail.com>
2023-05-04 01:20:30 +0100

    add link-button  utility component

--- src/utils/LinkButton.vue
+++ src/utils/LinkButton.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
+import { computed} from "vue";
+
 interface Props {
   linkTo: string;
   linkDisplay: string;
   linkName: string;
+  isFormControl?: boolean;
 }
 
-const { linkDisplay, linkName, linkTo } = defineProps<Props>();
+const { linkDisplay, linkName, linkTo, isFormControl } = defineProps<Props>();
+
+const clsDisplay = `btn btn btn-${linkDisplay} m-1 fw-bold`
+
+const display = computed(() => {
+  return !isFormControl?  clsDisplay : `${clsDisplay} form-control`;
+});
 </script>
 
 <template>
-  <router-link :to="linkTo" :class="`btn btn-${linkDisplay} m-1 fw-bold`" >{{
-    linkName
-  }}</router-link>
+  <router-link :to="linkTo" :class="display"> {{ linkName }}</router-link>
 </template>
---
+++ src/views/authors/DetailAuthorView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { useFetch } from "@/composables/useFetch";
+import type AuthorDto from "@/models/authors/author.model";
+import { authorUrl } from "@/urls/author.url";
+import { useRoute, useRouter } from "vue-router";
+import ChangePasswordForm from "@/components/forms/auth/ChangePasswordForm.vue";
+import ListDepartmentView from "@/views/departments/ListDepartmentView.vue";
+import ChangePasswordView from "@/views/auth/ChangePasswordView.vue";
+
+const { id } = useRoute().params;
+
+const url = `${authorUrl}/${id}`;
+
+const { resource: author } = useFetch<AuthorDto>(url);
+</script>
+
+<template>
+  <div class="border pado">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="text-center">Author Detail</h4>
+      </div>
+      <div class="card-body">
+        <ul class="list-group">
+          <li class="list-group-item">name: <strong>{{ author.name }}</strong></li>
+          
+        </ul>
+      </div>
+      <div class="card-footer">
+            
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.pado {
+  padding: 10px;
+}
+</style>
